44d0bd76fac812d979d0a2e46661f1205a4f5672
thierry thomassin-simard <thierry.thomassin001@hotmail.fr>
2024-05-14 10:55:03 -0400

    commit

--- TP3/media/MediaPlayer.cs
+++ TP3/media/MediaPlayer.cs
@@ -112,9 +112,7 @@ namespace PROF.media
                     {
                         unusedMedias.Add(media);
                     }
-                }
-
-                
+                }                
             }
             return unusedMedias;
         }
